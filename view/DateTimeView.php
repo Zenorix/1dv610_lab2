578bde7b46ca5cf9cc02ffde15262faf7475f2d9
ei222bn <ei222bn@student.lnu.se>
2019-09-11 21:43:03 +0300

    Completed test case 1.1

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -2,10 +2,11 @@
 
 class DateTimeView {
 
-
+	// [Day of week], the [day of month numeric]th of [Month as text] [year 4 digits].The time is [Hour]:[minutes]:[Seconds].
+	// Example: "Monday, the 8th of July 2015, The time is 10:59:21".
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString =  date("l, \\t\h\\e jS \of F Y, \T\i\m\\e \i\s H:i:s");
 
 		return '<p>' . $timeString . '</p>';
 	}
