af140e7772dc575eac0b1f85cb1ada8295075919
ei222bn <ei222bn@student.lnu.se>
2019-09-26 23:09:56 +0300

    Correction - time output with capital T

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ namespace View;
 class DateTimeView {
     // [Day of week], the [day of month numeric]th of [Month as text] [year 4 digits].The time is [Hour]:[minutes]:[Seconds].
     // Example: "Monday, the 8th of July 2015, The time is 10:59:21".
-    private $formatString = 'l, \\t\\h\\e jS \\of F Y, \\t\\h\\e \\t\\i\\m\\e \\i\\s H:i:s';
+    private $formatString = 'l, \\t\\h\\e jS \\of F Y, \\T\\h\\e \\t\\i\\m\\e \\i\\s H:i:s';
 
     public function generateHTML() {
         $dateString = date($this->formatString);
